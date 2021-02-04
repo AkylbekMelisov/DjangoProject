efd370cfb9e0e76c50996f6641053913ae1d89eb
Melisov Akylbek <akylbek.melisov@gmail.com>
2021-02-04 17:21:10 +0600

    practik task

---
+++ pizza/pizza/task1.py
@@ -0,0 +1,20 @@
+customers = ['brad','jolie','johny','sooronbai','sooronbai','sadyr','sadyr','emma','sooronbai',
+         'brad','almazbek','roza','roza','kurmanbek','toby','tony','robert','askar','robert','chris','chris']
+
+
+
+dict1 = {}
+
+for i in customers:
+    dict1[i] = customers.count(i)
+print(dict1)
+
+
+import random
+
+r = random.sample(range(1,100000),60000)
+
+if r.index(max(r)) > r.index(min(r)):
+    print(r.index(max(r)) - (r.index(min(r))))
+else:
+    print((r.index(min(r))) - (r.index(max(r))))
